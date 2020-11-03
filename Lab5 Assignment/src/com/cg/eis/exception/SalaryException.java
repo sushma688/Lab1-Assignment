745880040a2d6046cc93bfd36aabc92e056e960a
sushma688 <73290568+sushma688@users.noreply.github.com>
2020-11-03 07:15:27 +0530

    Lab5 Assignment

---
+++ Lab5 Assignment/src/com/cg/eis/exception/SalaryException.java
@@ -0,0 +1,10 @@
+package com.cg.eis.exception;
+
+public class SalaryException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public SalaryException() { // class constructor
+		super("Your salary below than 3000 "); // call super class
+	}
+
+}
